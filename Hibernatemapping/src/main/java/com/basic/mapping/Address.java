687b47ba9bc45478bb6edd5ac9e4edc8d63b25a6
NilayRai <40742612+NilayRai@users.noreply.github.com>
2018-10-11 13:50:10 +0530

    Update Address.java

--- Hibernatemapping/src/main/java/com/basic/mapping/Address.java
+++ Hibernatemapping/src/main/java/com/basic/mapping/Address.java
@@ -1,13 +1,14 @@
 package com.basic.mapping;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-
+@Table
 public class Address {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
